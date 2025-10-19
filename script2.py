82eaed444e0a49ac3055844f28ad462152a20574
CounterDark <aleksanderkunkowski@gmail.com>
2025-10-19 09:02:10 +0200

    small fixes

--- script2.py
+++ script2.py
@@ -14,8 +14,8 @@ def getArgs():
 
 
 def getModel(x_train, y_train):
-    if Path("model2.keras").exists():
-        model = tf.keras.models.load_model("model2.keras")
+    if Path("saved/model2.keras").exists():
+        model = tf.keras.models.load_model("saved/model2.keras")
     else:
         model = tf.keras.models.Sequential(
             [
@@ -30,6 +30,24 @@ def getModel(x_train, y_train):
             loss="sparse_categorical_crossentropy",
             metrics=["accuracy"],
         )
+        history = model.fit(x_train, y_train, epochs=10, validation_split=0.2)
+        plt.figure()
+        plt.plot(history.history["accuracy"], label="train_accuracy")
+        plt.plot(history.history["val_accuracy"], label="val_accuracy")
+        plt.xlabel("Epoch")
+        plt.ylabel("Accuracy")
+        plt.legend()
+        plt.show()
+
+        plt.figure()
+        plt.plot(history.history["loss"], label="train_loss")
+        plt.plot(history.history["val_loss"], label="val_loss")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.legend()
+        plt.show()
+        plt.imshow(x_train[0].reshape(28, 28))
+        plt.show()
     return model
 
 
@@ -39,25 +57,8 @@ def main():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
     model = getModel(x_train, y_train)
-    history = model.fit(x_train, y_train, epochs=10, validation_split=0.2)
 
     # print(history.history.keys())
-
-    plt.figure()
-    plt.plot(history.history["accuracy"], label="train_accuracy")
-    plt.plot(history.history["val_accuracy"], label="val_accuracy")
-    plt.xlabel("Epoch")
-    plt.ylabel("Accuracy")
-    plt.legend()
-    plt.show()
-
-    plt.figure()
-    plt.plot(history.history["loss"], label="train_loss")
-    plt.plot(history.history["val_loss"], label="val_loss")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.show()
 
     # model.evaluate(x_test, y_test)
 
@@ -69,11 +70,13 @@ def main():
         )
         input_arr = tf.keras.utils.img_to_array(img) / 255.0
         input_arr = np.array([input_arr])  # Convert single image to a batch.
+        plt.imshow(input_arr.reshape(28, 28))
+        plt.show()
         predictions = model.predict(input_arr)
         print(predictions.argmax())
 
     # model.summary()
-    model.save("model2.keras")
+    model.save("saved/model2.keras")
 
 
 if __name__ == "__main__":
